532e178196954529a87594f3537e6ec3b72994eb
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-29 17:31:16 -0500

    :wrench: Add Webpack Production Config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+
+const Dotenv = require('dotenv-webpack')
+const HtmlPlugin = require('html-webpack-plugin')
+
+/** @param {string} dir */
+const fm = (dir) => path.join(__dirname, dir)
+
+module.exports = {
+  entry: './src/index.js',
+
+  mode: 'production',
+
+  resolve: {
+    alias: {
+      '@components': fm('src/components'),
+      '@containers': fm('src/containers'),
+      '@utils': fm('src/utils'),
+      '@config': fm('src/config'),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new Dotenv({
+      path: './.env',
+      safe: true,
+      allowEmptyValues: true,
+      systemvars: true,
+      silent: true,
+      defaults: false,
+    }),
+    new HtmlPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+}
