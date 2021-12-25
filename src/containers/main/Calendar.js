04f1684b29d2b9d0eea1019a6a24153e87fc7ad3
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-12-25 09:23:38 -0500

    :bug: FIX bug

--- src/containers/main/Calendar.js
+++ src/containers/main/Calendar.js
@@ -1,32 +1,35 @@
-import React, { useState } from 'react'
+/* eslint-disable no-nested-ternary */
+
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 import { Calendar as StyledCalendar } from '@components'
 import { dateDiffInDays } from '@utils'
 
 const Calendar = ({ dateQuery }) => {
-  const [date, setDate] = useState(dateQuery ? new Date(dateQuery) : null)
+  const [date, setDate] = useState(null)
+
+  useEffect(() => {
+    if (dateQuery) setDate(new Date(dateQuery))
+  }, [dateQuery])
 
   return (
     <>
       <StyledCalendar
         onChange={(event) => setDate(new Date(event.target.value))}
         value={
-          date instanceof Date
-            ? `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
-            : ''
+          date instanceof Date ??
+          `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
         }
       />
 
-      {date instanceof Date ? (
-        <p>
-          {dateDiffInDays(date) >= 0
+      <p>
+        {date instanceof Date
+          ? dateDiffInDays(date) >= 0
             ? `${dateDiffInDays(date)} days left for ${date.toString()}.`
-            : `${dateDiffInDays(date)
-                .toString()
-                .replace('-', '')} days have passed since ${date.toString()}.`}
-        </p>
-      ) : null}
+            : `${dateDiffInDays(date)} days passed since ${date.toString()}.`
+          : null}
+      </p>
     </>
   )
 }
