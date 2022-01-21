0b561d4e940dd889cb9ca0dab169345b7da6f672
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-01-21 09:56:33 -0500

    deploy

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Header, Text } from '@components'
+import { Header } from '@components'
 
 export default function NotFound() {
   return (
     <>
       <Header text="404" />
       <div style={{ textAlign: 'center' }}>
-        <Text>Page not found</Text>
+        <p>Page not found</p>
       </div>
     </>
   )
