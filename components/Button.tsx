901c2e474fd347afa3881e1bf4200eb2a8b90ac3
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-12 14:32:54 -0500

    :recycle: Update

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,9 +6,9 @@ export function Button() {
     <button
       type="submit"
       class={tw
-        `text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800bg-indigo-500`}
+        `text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800bg-indigo-500`}
     >
-      Search
+      Go!
     </button>
   );
 }
--- components/Head.tsx
+++ components/Head.tsx
@@ -2,13 +2,13 @@
 import { h, Head } from "../client_deps.ts";
 
 export function DiffTimeHead() {
-  const titleApp = "Diff Time";
-
-  const [questionOne, questionTwo, ...rest] = [
+  const [titleApp, questionOne, questionTwo, ...rest] = [
+    "Diff Time",
     "How much time since",
     "How much time until",
     "How much time is left",
     "How much time is left until",
+    "How much time is left since",
   ];
 
   return (
@@ -20,6 +20,7 @@ export function DiffTimeHead() {
         content={`${titleApp}, ${questionOne} X? ${questionTwo} X?`}
       />
       <meta name="author" content="Eliaz Bobadilla" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       <meta
         name="keywords"
         content={[questionOne, questionTwo, ...rest].join(", ")}
